87d434bf662bb00744fa59613e0698e6d1a5334f
Kevin Kobori <kevinyujikobori@gmail.com>
2025-07-15 22:56:12 -0300

    remove demo and documentation unnecessary projects

--- demo/android/app/src/main/kotlin/com/fluttely/material_toolkit_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fluttely.material_toolkit_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- documentation/android/app/src/main/kotlin/com/fluttely/material_toolkit_documentation/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fluttely.material_toolkit_documentation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
